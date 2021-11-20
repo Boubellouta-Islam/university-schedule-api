8aa7e5bbdd56c2b9cb8b1c6410a6796a3897a4b4
unknown <abdelhaibouaocha@gmail.com>
2021-11-20 15:44:04 +0100

    adding the needed enums

--- src/utils/enum.ts
+++ src/utils/enum.ts
@@ -16,3 +16,52 @@ export enum Teaching_unit {
   UEM = 'UE Méthodologie',
   UET = 'UE Transversale',
 }
+
+export enum ClassType {
+  COURS = 'cours',
+  TD = 'td',
+  TP = 'tp',
+}
+
+export enum LavelName {
+  L1 = 'l1',
+  L2 = 'l2',
+  L3 = 'l3',
+  M1 = 'm1',
+  M2 = 'm2',
+}
+
+export enum Domain {
+  MATH_INFO = 'math & info',
+}
+
+export enum Filiere {
+  INF = 'informatique',
+}
+
+export enum Specialty {
+  FND = 'fondamentale',
+  IND = 'industrielle',
+  ASR = 'administration et sécurité de réseau',
+  SI = "système d'information",
+}
+
+export enum Day {
+  SAT = 'sat',
+  SUN = 'sun',
+  MON = 'mon',
+  TUE = 'tue',
+  WED = 'wed',
+  THU = 'thu',
+}
+
+export enum Status {
+  FREE = 'free',
+  BUSSY = 'bussy',
+  NOT_SURE = 'not sure',
+}
+
+export enum VoeuxType {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+}
