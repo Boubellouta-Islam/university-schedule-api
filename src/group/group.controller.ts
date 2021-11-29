3aa7b3fed6ec4b8a7b9d098fe96c4e4828c549aa
abderrahman_riag <66516219+abderrahmanriag@users.noreply.github.com>
2021-11-29 12:02:52 +0100

    commit
    riag abderrahman

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Put, Body, Param, Get, Delete} from '@nestjs/common';
 import { GroupService } from './group.service';
+import { CreateGroup } from './group.dto';
 
 @Controller('group')
 export class GroupController {
   constructor(private readonly groupService: GroupService) {}
+
+  @Post()
+  create(@Body()createGroup: CreateGroup): string{
+    return `Name: ${createGroup.name}`;
+  }
+  @Get()
+  findAll(): string{
+    return 
+  }
+
+  @Put(':id')
+  update(@Body()updateGroup: CreateGroup, @Param('id') id): string{
+    return `Update ${id} - Name ${updateGroup.name}`;
+  }
+
+  @Delete(`:id`)
+  delete(@Param(`:id`) id):string{
+    return `Delete ${id}`;
+  }
 }
---
+++ src/group/group.dto.ts
@@ -0,0 +1,5 @@
+export class CreateGroup{
+    readonly id:number;
+    readonly name:string;
+    readonly student_num:number;
+}
