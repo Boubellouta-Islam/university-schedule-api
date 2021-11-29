ab882a4c6d46fca150f7ab3dfe1ed4d5c557cb10
unknown <abdelhaibouaocha@gmail.com>
2021-11-29 18:35:23 +0100

    oualids task

--- src/affectation/affectation.service.ts
+++ src/affectation/affectation.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Subject } from 'src/subject/subject.schema';
+import { Teacher } from 'src/teacher/teacher.schema';
+import AffectionDto from './affectation.dto';
 import { AffecationDocument, Affectation } from './affectation.schema';
 
 @Injectable()
@@ -9,4 +12,57 @@ export class AffectationService {
     @InjectModel(Affectation.name)
     private affectationModel: Model<AffecationDocument>,
   ) {}
+
+  async findAllAffection() {
+    return this.affectationModel.find();
+  }
+
+  async findByTeacherId(teacher: Teacher) {
+    const post = await this.affectationModel.find({ teacher: teacher });
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  async findBySubjectId(subject: Subject) {
+    const post = await this.affectationModel.find({ subject: subject });
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  async findBySubjectTeacher(teacher: Teacher, subject: Subject) {
+    const post = await this.affectationModel.find({
+      teacher: teacher,
+      subject: subject,
+    });
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  async createAffection(Data: AffectionDto) {
+    const createdPost = new this.affectationModel(Data);
+    return createdPost.save();
+  }
+
+  async updateAffection(id: string, Data: AffectionDto) {
+    const post = await this.affectationModel
+      .findByIdAndUpdate(id, Data)
+      .setOptions({ overwrite: true, new: true });
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  async deleteAffection(affectionId: string) {
+    const result = await this.affectationModel.findByIdAndDelete(affectionId);
+    if (!result) {
+      throw new NotFoundException();
+    }
+  }
 }
